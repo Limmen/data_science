c68ecdce7107f06076a960fb03df07d980e58df7
Kim Hammar <kimham@kth.se>
2017-08-24 15:16:06 +0200

    add erlang kmeans and naive-bayes implementations

---
+++ erl_kmeans/erl_kmeans.erl
@@ -0,0 +1,144 @@
+%%%-------------------------------------------------------------------
+%% @author Kim Hammar <kimham@kth.se>
+%% @copyright (C) 2017, Kim Hammar
+%% @doc erl_kmeans.erl
+%% Example use-case:
+%% > c(erl_kmeans).
+%% > Data = erl_kmeans:read_data("sample_kmeans_data.txt").
+%% > Clusters = erl_kmeans:run_k_means(2, 50, Data).
+%% @end
+%%%-------------------------------------------------------------------
+-module(erl_kmeans).
+-author('Kim Hammar <kimham@kth.se>').
+
+%% API
+-export([read_data/1, run_k_means/3, print_clusters/1]).
+
+%% types
+
+-type cluster():: list(feature_vector()).
+
+-type data_points() :: list(feature_vector()).
+
+-type feature_vector() :: list().
+
+-type clusters():: list(cluster()).
+
+-type centroids() :: list(feature_vector()).
+
+
+
+%%====================================================================
+%% API functions
+%%====================================================================
+
+-spec read_data(FileName::string())->list(list()).
+read_data(FileName)->
+    Lines = read_input_lines(FileName),
+    parse_input(Lines).
+
+%% k-means algorithm given K, Number of Iterations and DataPoints
+-spec run_k_means(K :: integer(), Iterations :: integer(), FeatureVectors :: data_points()) -> Clusters :: clusters().
+run_k_means(K, Iterations, FeatureVectors)->
+    io:format("--- K-MEANS Algorithm starting ---~n K : ~p, Iterations : ~p~n", [K,Iterations]),
+    Centroids = initial_centroids(K, FeatureVectors),
+    io:format("Initial Centroids: ~p~n", [Centroids]),
+    Clusters = clusters(Centroids, FeatureVectors),
+    io:format("Initial clusters ~p~n", [Clusters]),
+    Clusters2 = iterations(Iterations, Iterations, Clusters, FeatureVectors),
+    print_clusters(Clusters2),
+    Clusters2.
+
+%% pretty print clusters
+print_clusters(Clusters)->
+    lists:map(fun(I) ->
+		      io:format("Cluster ~p: ~p ~n", [I, lists:nth(I, Clusters)])
+	      end, lists:seq(1, length(Clusters))),
+    io:format("~n").
+
+
+%%====================================================================
+%% Internal functions
+%%====================================================================
+
+%% Get clusters given Centroids and DataPoints
+-spec clusters(Centroids :: centroids(), Data :: data_points()) -> Clusters :: clusters().
+clusters(Centroids, Data)->
+    InitMapping = lists:map(fun(C) -> {C, []} end, Centroids),
+    MappedToCentroids = lists:foldl(fun(DataPoint, ClustersAcc) ->
+					    Cluster = least_squares_error(DataPoint, Centroids),
+					    {Cluster, Members} = lists:keyfind(Cluster, 1, ClustersAcc),
+					    lists:keyreplace(Cluster, 1, ClustersAcc, {Cluster, [DataPoint|Members]})
+				    end, InitMapping, Data),
+    Clusters = lists:map(fun({_, Cluster}) -> Cluster end, MappedToCentroids),
+    Clusters.
+
+%% Computes the best centroid for a given datapoint
+-spec least_squares_error(DataPoint :: feature_vector(), Centroids :: centroids()) -> Centroid :: feature_vector().
+least_squares_error(DataPoint, Centroids)->
+    SquaredErrors = lists:map(fun(C) -> {euclidean_distance(DataPoint, C), C} end, Centroids),
+    {_, C} = lists:min(SquaredErrors),
+    C.
+
+%% Computes euclidean distance between two datapoints
+-spec euclidean_distance(Q :: feature_vector(), P :: feature_vector()) -> float().
+euclidean_distance(Q, P)->
+    SumSquaredErrors = lists:sum(lists:map(fun(I) -> math:pow(lists:nth(I, Q) - lists:nth(I, P), 2) end, lists:seq(1, length(Q)))),
+    math:sqrt(SumSquaredErrors).
+
+%% Performs N iterations of hte kmeans algorithm given initial clusters
+-spec iterations(N :: integer(), MaxIterations :: integer(), Clusters :: clusters(),  Data :: data_points()) -> Clusters :: clusters().
+iterations(0, _, Clusters, _)->
+    io:format("All iterations finished ~n~n"),
+    Clusters;
+
+iterations(N, Max, Clusters, Data) when N > 0 ->
+    Centroids = average_centroids(Clusters),
+    io:format("Iteration ~p, new centroids: ~p ~n", [Max - N+1, Centroids]),
+    iterations(N-1, Max, clusters(Centroids, Data), Data).
+
+%% Compute new average centroids based on given clusters
+-spec average_centroids(Clusters :: clusters()) -> Centroids :: centroids().
+average_centroids(Clusters) ->
+    lists:map(fun(C) ->
+		      average(C)
+	      end, Clusters).
+
+%% Computes average of a cluster
+-spec average(Cluster :: cluster()) -> AveragedCluster :: cluster().
+average(Cluster)->
+    SummedCluster = lists:foldl(fun(Member, Acc) ->
+					lists:foldl(fun(I, Acc2) ->
+							    case Acc of
+								[] ->
+								    Sum = lists:nth(I, Member),
+								    [Sum|Acc2];
+								_ ->
+								    Sum = lists:nth(I, Member) + lists:nth(I, Acc),
+								    [Sum|Acc2]
+							    end
+						    end, [], lists:seq(1, length(Member))) end, [], Cluster),
+    AveragedCluster = lists:map(fun(C) -> C/length(Cluster) end, SummedCluster),
+    AveragedCluster.
+
+%% Reads input file into list of lines
+-spec read_input_lines(FileName::string())-> ListOfLined :: list().
+read_input_lines(FileName)->
+    {ok, Binary} = file:read_file(FileName),
+    string:tokens(erlang:binary_to_list(Binary), "\n").
+
+%% Parses list of lines into feature-vectors
+-spec parse_input(FileName::list)-> ParsedInput :: data_points().
+parse_input(Lines)->
+    lists:map(fun(Line) ->
+		      lists:map(fun(X) -> list_to_float(X) end,
+				string:tokens(Line, " ")) end, Lines).
+
+%% Computes initial centroids by picking random datapoints
+-spec initial_centroids(K::integer(), Data::list(list())) -> Centroids :: list(list()).
+initial_centroids(K, Data)->
+    {Centroids, _} = lists:foldl(fun(_, {Centroids, DataAcc}) ->
+			Centroid = lists:nth(rand:uniform(length(DataAcc)), DataAcc),
+			{[Centroid|Centroids], lists:delete(Centroid, DataAcc)}
+		       end, {[], Data}, lists:seq(1, K)),
+    Centroids.
---
+++ erl_naive_bayes/erl_naive_bayes.erl
@@ -0,0 +1,274 @@
+%%%-------------------------------------------------------------------
+%% @author Kim Hammar <kimham@kth.se>
+%% @copyright (C) 2017, Kim Hammar
+%% @doc erl_naive_bayes.erl
+%% Example use-case:
+%% > c(erl_naive_bayes).
+%% > Examples = erl_naive_bayes:examples_play_tennis().
+%% > Model = erl_naive_bayes:learn_model(Examples).
+%% > erl_naive_bayes:classify(Model, [{outlook, overcast}, {temperature, hot}, {humidity, high}, {windy, true}]).
+%% > erl_naive_bayes:classify(Model, [{outlook, overcast}]).
+%% > erl_naive_bayes:classify(Model, [{outlook, sunny}, {humidity, high}]).
+%% @end
+%%%-------------------------------------------------------------------
+-module(erl_naive_bayes).
+-author('Kim Hammar <kimham@kth.se>').
+
+%% API
+-export([learn_model/1, examples_play_tennis/0, classify/2]).
+
+%% types
+
+-type model()::{frequency_table(), likelihood_table(), conditional_probability_table(), classes()}.
+
+-type conditional_probability_table()::list({attribute_value_pair(), classification(), float()}).
+
+-type likelihood_table()::list({attribute_value_pair() | classification(), float()}).
+
+-type frequency_table()::list(frequency_table_row()).
+
+-type frequency_table_row()::{attribute(), attribute_table()}.
+
+-type attribute_table()::list(attribute_table_row()).
+
+-type attribute_table_row()::{attribute_value(), list({classification(), integer()})}.
+
+-type attribute_value_pairs()::list(attribute_value_pair()).
+
+-type attribute_value_pair()::{attribute(), attribute_value()}.
+
+-type attribute():: atom().
+
+-type attributes():: list(attribute()).
+
+-type attribute_value():: atom().
+
+-type values():: values().
+
+-type classification():: atom().
+
+-type classes():: list(classification()).
+
+-type example():: {attribute_value_pairs(), classification()}.
+
+-type examples() :: list(example()).
+
+%%====================================================================
+%% API functions
+%%====================================================================
+
+%% Learn the naive-bayes model given a set of examples
+-spec learn_model(Examples :: examples()) -> model().
+learn_model(Examples) ->
+    FreqTable = create_frequency_table(Examples),
+    LikelihoodTable = create_likelihood_table(Examples),
+    ConditionalProbabilityTable = create_conditional_probability_table(Examples, FreqTable),
+    Classes = sets:to_list(sets:from_list(classes(Examples))),
+    {FreqTable, LikelihoodTable, ConditionalProbabilityTable, Classes}.
+
+%% Classify a set of attributes to a given class given a Model
+-spec classify(Model::model(), Evidence::attribute_value_pairs())-> {ClassProbability :: float(), Class :: classification()}.
+classify({_,LikelihoodTable,ConditionalProbabilityTable,Classes}, Evidence)->
+    PosteriorProbabilities = lists:map(fun(C) -> {posterior_probability(C, Evidence, LikelihoodTable, ConditionalProbabilityTable), C} end, Classes),
+    {ClassProb, Class} = lists:max(PosteriorProbabilities),
+    {ClassProb, Class}.
+
+%%====================================================================
+%% Internal functions
+%%====================================================================
+
+%% Calculate Posterior probability of Class given evidence and Conditional probabilities
+-spec posterior_probability(Class::classification(), Evidence::attribute_value_pairs(), LikelihoodTable::likelihood_table(), ConditionalProbabilityTable::conditional_probability_table()) -> PosteriorProbability::float().
+posterior_probability(Class, Evidence, LikelihoodTable, ConditionalProbabilityTable)->
+    {Class, ClassProb} = lists:keyfind(Class, 1, LikelihoodTable),
+    ConditionalProbList = lists:map(fun(AV) ->
+					    {AV, Class, AttributeValueProb} = lists:keyfind(Class, 2, lists:filter(fun({AV2, _, _})-> AV =:= AV2 end, ConditionalProbabilityTable)),
+					    AttributeValueProb
+				    end, Evidence),
+    ConditionalProb = lists:foldl(fun(P, Acc) ->
+					  P*Acc
+				  end, ClassProb, ConditionalProbList),
+    EvidenceProbList = lists:map(fun(AV) ->
+					 {AV, AVProb} = lists:keyfind(AV, 1, LikelihoodTable),
+					 AVProb
+				 end, Evidence),
+    EvidenceProb = lists:foldl(fun(P, Acc) ->
+				       P*Acc
+			       end, 1, EvidenceProbList),
+    ConditionalProb/EvidenceProb.
+%% Create conditional probability table for given exampels and frequency table
+-spec create_conditional_probability_table(Examples:: examples(), FreqTable::frequency_table()) -> conditional_probability_table().
+create_conditional_probability_table(Examples, FreqTable)->
+    AttributeValues = sets:to_list(sets:from_list(attribute_value_pairs(Examples))),
+    Classes = sets:to_list(sets:from_list(classes(Examples))),
+    lists:flatten(lists:foldl(fun(AV, Acc) ->
+				      ConditionalProbabilityList = lists:foldl(fun(C, Acc2) ->
+										       [{AV, C, conditional_probability(AV, C, FreqTable)}|Acc2]
+									       end, [], Classes),
+				      [ConditionalProbabilityList|Acc]
+			      end, [], AttributeValues)).
+
+%% Calculate conditional probability for Attribute-Value pair given Class
+-spec conditional_probability(attribute_value_pair(), classification(), frequency_table()) -> ConditionalProbability::float().
+conditional_probability({Attribute, Value}, Class, FreqTable)->
+    {all, L} = lists:keyfind(all, 1, FreqTable),
+    {Class, [{Class, ClassFreq}]} = lists:keyfind(Class, 1, L),
+    {Attribute, AttributeTable} = lists:keyfind(Attribute, 1, FreqTable),
+    {Value, ValueClassCountList} = lists:keyfind(Value, 1, AttributeTable),
+    {Class, AttributeValueGivenClassFreq} = lists:keyfind(Class, 1, ValueClassCountList),
+    AttributeValueGivenClassFreq/ClassFreq.
+
+%% Create likelihoodTable given a set of examples
+-spec create_likelihood_table(Examples :: examples) -> likelihood_table().
+create_likelihood_table(Examples) ->
+    AttributeValues = attribute_value_pairs(Examples),
+    Classes = classes(Examples),
+    ClassesAttributes = Classes ++ AttributeValues,
+    ClassesAttributesSet = sets:to_list(sets:from_list(ClassesAttributes)),
+    LikelihoodTable = lists:map(fun(CA) ->
+					Count = lists:foldl(fun(CA2, Acc) ->
+								    case CA2 of
+									CA ->
+									    Acc + 1;
+									_ ->
+									    Acc
+								    end
+							    end, 0, ClassesAttributes),
+					Probability = Count / length(Examples),
+					{CA, Probability}
+				end, ClassesAttributesSet),
+    LikelihoodTable.
+
+%% Create frequency table given a set of examples
+-spec create_frequency_table(Examples :: examples()) -> frequency_table().
+create_frequency_table(Examples)->
+    Attributes = attributes(Examples),
+    FreqAttributes = lists:foldl(fun(A, Acc) ->
+					 [{A, create_attribute_table(A,Examples)}|Acc]
+				 end, [], sets:to_list(sets:from_list(Attributes))),
+    Classes = classes(Examples),
+    FreqTotal = {all, lists:map(fun(C) ->
+					Count = length(lists:filter(fun(C2) -> C2 =:= C end, Classes)),
+					{C, [{C, Count}]}
+				end, sets:to_list(sets:from_list(Classes)))},
+    FreqAttributes ++ [FreqTotal].
+
+%% Create attribute table for a given attribute and set of examples
+-spec create_attribute_table(Attribute :: attribute(), Examples :: examples()) -> attribute_table().
+create_attribute_table(Attribute, Examples)->
+    Values = values(Examples, Attribute),
+    lists:foldl(fun(V, Acc) ->
+			[{V, count_classifications_for_attribute_value(Attribute, V, Examples)}|Acc]
+		end, [], sets:to_list(sets:from_list(Values))).
+
+%% Count classifications per value for a given attribute
+-spec count_classifications_for_attribute_value(Attribute::attribute(), Value :: attribute_value(), Examples :: examples) -> attribute_table_row().
+count_classifications_for_attribute_value(Attribute, Value, Examples)->
+    Classes = classes(Examples),
+    CountedClasses = lists:map(fun(C) ->
+				       Count = lists:foldl(fun
+							       ({AVs, C2}, Acc) ->
+								  case C2 of
+								      C ->
+									  case lists:member({Attribute, Value}, AVs) of
+									      true ->
+										  Acc + 1;
+									      false ->
+										  Acc
+									  end;
+								      _ ->
+									  Acc
+								  end
+							  end, 0, Examples),
+				       {C, Count}
+			       end, sets:to_list(sets:from_list(Classes))),
+    CountedClasses.
+
+
+%% Extract list of classes from Examples
+-spec classes(Examples :: examples()) -> Classes::classes().
+classes(Examples)->
+    lists:map(fun({_, C}) -> C end, Examples).
+
+%% Extract values for a given attribute from Examples of attribute-value pairs + classification
+-spec values(Examples :: examples(), Attribute :: attribute()) -> Values::values().
+values(Examples, Attribute)->
+    lists:map(fun({_,V}) -> V end,
+	      lists:filter(fun({A,_}) -> A =:= Attribute end,
+			   attribute_value_pairs(Examples))).
+
+%% Extract attribute-value-pairs from Examples
+-spec attribute_value_pairs(Examples :: examples()) -> AttributeValues::attribute_value_pairs().
+attribute_value_pairs(Examples)->
+    lists:flatten(lists:map(fun({A, _}) -> A end, Examples)).
+
+%% Extract attributes from Examples
+-spec attributes(Examples :: examples()) -> Attributes::attributes().
+attributes(Examples)->
+    lists:map(fun({A,_}) -> A end, lists:flatten(lists:map(fun({A, _}) -> A end, Examples))).
+
+%%====================================================================
+%% Example Data
+%%====================================================================
+
+%% Sample set of examples
+-spec examples_play_tennis() -> examples().
+examples_play_tennis()->
+    [
+     {[
+       {outlook,sunny},{temperature,hot},{humidity,high},{windy,false}
+      ], not_play
+     },
+     {[
+       {outlook,sunny},{temperature,hot},{humidity,high},{windy,true}
+      ], not_play
+     },
+     {[
+       {outlook,overcast},{temperature,hot},{humidity,high},{windy,false}
+      ], play
+     },
+     {[
+       {outlook,rain},{temperature,mild},{humidity,high},{windy,false}
+      ], play
+     },
+     {[
+       {outlook,rain},{temperature,cool},{humidity,normal},{windy,false}
+      ], play
+     },
+     {[
+       {outlook,rain},{temperature,cool},{humidity,normal},{windy,true}
+      ], not_play
+     },
+     {[
+       {outlook,overcast},{temperature,cool},{humidity,normal},{windy,true}
+      ], play
+     },
+     {[
+       {outlook,sunny},{temperature,mild},{humidity,high},{windy,false}
+      ], not_play
+     },
+     {[
+       {outlook,sunny},{temperature,cool},{humidity,normal},{windy,false}
+      ], play
+     },
+     {[
+       {outlook,rain},{temperature,mild},{humidity,normal},{windy,false}
+      ], play
+     },
+     {[
+       {outlook,sunny},{temperature,mild},{humidity,normal},{windy,true}
+      ], play
+     },
+     {[
+       {outlook,overcast},{temperature,mild},{humidity,high},{windy,true}
+      ], play
+     },
+     {[
+       {outlook,overcast},{temperature,hot},{humidity,normal},{windy,false}
+      ], play
+     },
+     {[
+       {outlook,rain},{temperature,mild},{humidity,high},{windy,true}
+      ], not_play
+     }
+    ].
